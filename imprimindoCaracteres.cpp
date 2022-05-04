e8617b677633081c36790daf4012c6e999929dce
Douglas Alves <dsilva@ufu.br>
2022-05-04 17:17:47 -0300

    Create imprimindoCaracteres.cpp
    exercices

---
+++ imprimindoCaracteres.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    system ("cls");//limpa a tela
+    cout << "\n\n";
+    //inicia a impressao do carro
+    cout << "\n\t\xDC\xDC\xDB\xDB\xDB\xDB\xDC\xDC";
+    cout << "\n\t\xDFO\xDF\xDF\xDF\xDFO\xDF";
+    //fIM DA IMPRESSAO DO CARRO
+    cout << "\n\n";
+    system("PAUSE");
+    return  0;
+
+}
